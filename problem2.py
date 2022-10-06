7adb62e185db582a361423a1a9c6a1b6f0b7beca
victor <victorphiathep@gmail.com>
2022-10-06 11:51:00 -0400

    Problem 2 Added

--- problem2.py
+++ problem2.py
@@ -1,27 +1,54 @@
-# file = open("nums.txt", 'r')
-# data = file.readlines()
-# count = 0
-# for line in data:
-#     s = line.split(' ')
-#     count += s.count('1')
-#     count += s.count('1\n')
-# print(count)
+def freq_dictionary(infile):
+    """
+        Takes a text file and creates a dictionary with key-value pairs
+        with the number of appearances of a word.
+    """
+
+    file = open(infile, 'r')
+    data = file.read()
+    freq_dict = {}
+
+    for word in data.split():
+        word = word.lower()
+        print(word)
+        if word not in freq_dict:
+            freq_dict[word] = 1
+        else:
+            freq_dict[word] += 1
+
+    # for line in data:
+    #     words = line.split(" ")
+    #     for word in words:
+    #         word = word.strip().lower()
+    #         print(word)
+    #         
+    
+    print(freq_dict)
+
+    file.close()
+    
+    return freq_dict
 
 
-#count = 0
-#
-#for line in sfile:
-#
- #   if len( line.split() ) == 0:
-#
- #       count += 1
-#
-#print(count)
 
-#sfile.close()
-sales = { 'apple': 2, 'orange': 3, 'grapes': 4 }
-print(sales.get(2))
-message = ""
-for key, val in sales.items():
-    message += f"{key}^{val} + "
-print(message.strip(" + "))
+def cosine_similarity(docfile1, docfile2):
+    """
+        Takes two text files, creates a dictionary of word frequencies,
+        and finds the cosine similarity of the two.
+    """
+    # Open both files for reading
+
+    # file_1 = open(docfile1, 'r')
+    # file_2 = open(docfile2, 'r')
+
+    file_1_dict = freq_dictionary(docfile1)
+    file_2_dict = freq_dictionary(docfile2)
+
+    # Cosine Similarity
+
+
+
+
+
+freq_dictionary("doc3.txt")
+# cosine_similarity('doc2.txt','doc3.txt')
